44e85fb30810fbc71b7f78d9c1353194440cc585
sherlock <920825209@qq.com>
2014-12-19 01:24:42 +0800

    ng-style表达式添加

--- js/controllers/home.ctrl.js
+++ js/controllers/home.ctrl.js
@@ -1,12 +1,16 @@
 wondervoy
 
-    .controller("HomeCtrl", function ($rootScope,$scope,$log,$q,$timeout,MessageService) {
+    .controller("HomeCtrl", function ($rootScope,$scope,$window,$log,$q,$timeout,MessageService) {
 
         //控制 loading
        $scope.isShow = false;
 
         //列表
         $scope.posts = [];
+        //height
+        $scope.style= {height: $window.innerWidth  / 2+'px'};
+
+
 
 
         //加载更多
--- js/controllers/main.ctrl.js
+++ js/controllers/main.ctrl.js
@@ -4,8 +4,16 @@ wondervoy
 
         //缩放比例
         var  scale = 2;
-        $scope.banner_height = $window.innerWidth  / 2;
-        console.log( $scope.win_height);
+//        $scope.banner_height = $window.innerWidth  / 2;
+//        console.log( $scope.win_height);
 
 
+
+
+
+//        //窗口变化
+//        $window.onResizeFunction = function(){
+//            $scope.style= {height:'100px'};
+//          console.log("resize..." + $scope.style.height);
+//        };
     });
